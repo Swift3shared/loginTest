2c0d834af8782f1a7f651779e906ea9a2dd6e226
sok channy <sokchanny@soks-mbp.kshrd.com.kh>
2016-11-08 10:10:54 +0700

    Initial Commit

---
+++ Login-homework-nini/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Login-homework-nini
+//
+//  Created by sok channy on 11/8/16.
+//  Copyright © 2016 pha vansa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
